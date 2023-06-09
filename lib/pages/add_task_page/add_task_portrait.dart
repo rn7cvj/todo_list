efc96b37b1ef8ec60edd9d608ade23b836653af6
rn7cvj <rn7cvj@outlook.com>
2023-06-10 03:36:08 +1000

    add inti add task apge

--- lib/pages/add_task_page/add_task_portrait.dart
+++ lib/pages/add_task_page/add_task_portrait.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/constants.dart';
+
+import '../../i18n/strings.g.dart';
 
 class AddTaskPortrait extends StatelessWidget {
   const AddTaskPortrait({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: appPaddingSmall),
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                t.common.save,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(appPaddingMedium),
+            color: Theme.of(context).colorScheme.secondaryContainer,
+            child: Padding(
+              padding: const EdgeInsets.all(appPaddingMedium),
+              child: TextField(
+                minLines: 4,
+                maxLines: 1000,
+                decoration: InputDecoration.collapsed(hintText: t.addtask.what_to_do),
+              ),
+            ),
+          ),
+          DropdownMenu(
+            dropdownMenuEntries: [],
+          )
+        ],
+      ),
+    );
   }
 }
